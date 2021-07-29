8b1d842297e5e8bfde726925b8bc0efd504aab8d
Sangjun Park <cosmoquester@gmail.com>
2021-07-29 23:34:40 +0900

    feat: Implement Pearson Correlation Loss
    Signed-off-by: Sangjun Park <cosmoquester@gmail.com>

--- transformers_tf_finetune/losses.py
+++ transformers_tf_finetune/losses.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from .metrics import pearson_correlation_coefficient
+
 
 class SparseCategoricalCrossentropy(tf.keras.losses.Loss):
     """Normal sparse categorical crossentrophy with ignore index"""
@@ -19,3 +21,15 @@ class SparseCategoricalCrossentropy(tf.keras.losses.Loss):
         loss = tf.keras.losses.sparse_categorical_crossentropy(y_true, y_pred, from_logits=self.from_logits)
         loss = tf.boolean_mask(loss, y_true != self.ignore_index)
         return loss
+
+
+class PearsonCorrelationLoss(tf.keras.losses.Loss):
+    """Loss function inversely propotional to PearsonCorrelation"""
+
+    def __init__(self, name="sparse_categorical_crossentropy"):
+        super().__init__(name=name, reduction=tf.keras.losses.Reduction.NONE)
+
+    def call(self, y_true, y_pred):
+        pearson_coef = pearson_correlation_coefficient(y_true, y_pred)
+        pearson_loss = 1 - pearson_coef
+        return pearson_loss
