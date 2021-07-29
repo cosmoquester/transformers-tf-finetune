fix: Use attention mask

scripts/train_chatbot.py
@@ -67,14 +67,14 @@ def load_dataset(dataset_path: str, tokenizer: AutoTokenizer, shuffle: bool = Fa
         answers.append(bos + answer + eos)
 
     max_length = max(len(text) for text in questions + answers)
-    input_tokens = tokenizer(
+    inputs = tokenizer(
         questions,
         padding="max_length",
         max_length=max_length,
         return_tensors="tf",
         return_token_type_ids=False,
-        return_attention_mask=False,
-    )["input_ids"]
+        return_attention_mask=True,
+    )
 
     target_tokens = tokenizer(
         answers,
@@ -86,7 +86,7 @@ def load_dataset(dataset_path: str, tokenizer: AutoTokenizer, shuffle: bool = Fa
     )["input_ids"]
 
     dataset = tf.data.Dataset.from_tensor_slices(
-        ({"input_ids": input_tokens, "decoder_input_ids": target_tokens[:, :-1]}, target_tokens[:, 1:])
+        ({**inputs, "decoder_input_ids": target_tokens[:, :-1]}, target_tokens[:, 1:])
     )
     return dataset, len(answers)
 

scripts/train_hate_speech.py
@@ -72,15 +72,17 @@ def load_dataset(dataset_path: str, tokenizer: AutoTokenizer, shuffle: bool = Fa
         bias_labels.append(bias_label2id[bias_label])
         hate_labels.append(hate_label2id[hate_label])
 
-    tokens = tokenizer(
-        sentences,
-        padding=True,
-        return_tensors="tf",
-        return_token_type_ids=False,
-        return_attention_mask=False,
-    )["input_ids"]
+    inputs = dict(
+        tokenizer(
+            sentences,
+            padding=True,
+            return_tensors="tf",
+            return_token_type_ids=False,
+            return_attention_mask=True,
+        )
+    )
 
-    dataset = tf.data.Dataset.from_tensor_slices(({"input_ids": tokens}, {"bias": bias_labels, "hate": hate_labels}))
+    dataset = tf.data.Dataset.from_tensor_slices((inputs, {"bias": bias_labels, "hate": hate_labels}))
     return dataset, len(sentences)
 
 

scripts/train_klue_nli.py
@@ -79,15 +79,17 @@ def load_dataset(
         sentences.append(start_token + example["premise"] + sep + example["hypothesis"] + end_token)
         labels.append(label2id[example["gold_label"]])
 
-    tokens = tokenizer(
-        sentences,
-        padding=True,
-        return_tensors="tf",
-        return_token_type_ids=False,
-        return_attention_mask=False,
-    )["input_ids"]
+    inputs = dict(
+        tokenizer(
+            sentences,
+            padding=True,
+            return_tensors="tf",
+            return_token_type_ids=False,
+            return_attention_mask=True,
+        )
+    )
 
-    dataset = tf.data.Dataset.from_tensor_slices(({"input_ids": tokens}, labels))
+    dataset = tf.data.Dataset.from_tensor_slices((inputs, labels))
     return dataset, len(labels)
 
 

scripts/train_klue_sts.py
@@ -85,14 +85,14 @@ def load_dataset(dataset_path: str, tokenizer: AutoTokenizer, shuffle: bool = Fa
         padding=True,
         return_tensors="tf",
         return_token_type_ids=False,
-        return_attention_mask=False,
+        return_attention_mask=True,
     )
     tokens2 = tokenizer(
         sentences2,
         padding=True,
         return_tensors="tf",
         return_token_type_ids=False,
-        return_attention_mask=False,
+        return_attention_mask=True,
     )
 
     dataset = tf.data.Dataset.from_tensor_slices(((dict(tokens1), dict(tokens2)), normalized_labels))