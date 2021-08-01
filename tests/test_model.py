79eefd314e77a71207c440ea7522a3c35177d199
Sangjun Park <cosmoquester@gmail.com>
2021-08-02 02:09:41 +0900

    test: Add test for GenerationSearchWrapper

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,9 @@
 import pytest
 import tensorflow as tf
-from transformers import BartConfig, TFBartModel
+from transformers import BartConfig, TFBartForConditionalGeneration, TFBartModel
 
 from transformers_tf_finetune.models import (
+    GenerationSearchWrapper,
     SemanticTextualSimailarityWrapper,
     TFBartClassificationHead,
     TFBartForSequenceClassification,
@@ -57,3 +58,25 @@ def test_semantic_textual_simailarity_wrapper(config: BartConfig):
     tf.debugging.assert_equal(tf.shape(output), [batch_size])
     tf.debugging.assert_less_equal(output, 1.0)
     tf.debugging.assert_greater_equal(output, -1.0)
+
+
+def test_generation_search_wrapper(config: BartConfig):
+    model = TFBartForConditionalGeneration(config)
+
+    batch_size = 4
+    encoder_sequence = 10
+    decoder_sequence = 15
+    bos_id = 2
+    eos_id = 3
+    max_sequence_length = 17
+
+    encoder_input = tf.random.uniform((batch_size, encoder_sequence), maxval=100, dtype=tf.int32)
+    attention_mask = tf.ones_like(encoder_input, dtype=tf.int32)
+    decoder_sequence = tf.random.uniform((batch_size, decoder_sequence), maxval=100, dtype=tf.int32)
+
+    searcher = GenerationSearchWrapper(model, max_sequence_length, bos_id, eos_id)
+    beam_result, beam_ppl = searcher.beam_search(encoder_input, attention_mask, 1)
+    greedy_result, greedy_ppl = searcher.greedy_search(encoder_input, attention_mask)
+
+    tf.debugging.assert_equal(beam_result[:, 0, :], greedy_result)
+    tf.debugging.assert_near(tf.squeeze(beam_ppl), greedy_ppl)
