00e0b74b64c512b2040975ab9f10ba18046cec8e
박상준 <cosmoquester@gmail.com>
2022-10-28 02:55:02 +0900

    style: Apply lint

--- scripts/train_klue_tc.py
+++ scripts/train_klue_tc.py
@@ -80,11 +80,7 @@ def load_dataset(
 
     inputs = dict(
         tokenizer(
-            sentences,
-            padding=True,
-            return_tensors="tf",
-            return_token_type_ids=False,
-            return_attention_mask=True,
+            sentences, padding=True, return_tensors="tf", return_token_type_ids=False, return_attention_mask=True,
         )
     )
 
